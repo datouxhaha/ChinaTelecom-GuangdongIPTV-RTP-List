2321b5a81e9f2e3b480d801531d02ad754b04921
datouxhaha <datouxp@outlook.com>
2026-05-03 14:48:01 +0800

    Replace rtp:// with http://192.168.9.1:8188/rtp/ for zubo

--- ffmpeg_scan.bat
+++ ffmpeg_scan.bat
@@ -3,7 +3,7 @@
 FOR /L %%i IN (0,1,1) DO (
   FOR /L %%j IN (1,1,255) DO (
     echo 239.77.%%i.%%j.
-    ffmpeg -i "rtp://239.77.%%i.%%j:5146" -vframes 1 %%i_%%j.jpeg >NUL 2>NUL
+    ffmpeg -i "http://192.168.9.1:8188/rtp/239.77.%%i.%%j:5146" -vframes 1 %%i_%%j.jpeg >NUL 2>NUL
 rem or
 rem ffmpeg -i "http://udpxy:port/rtp/239.77.%%i.%%j:5146" -vframes 1 %%i_%%j.jpeg >NUL 2>NUL
   )
